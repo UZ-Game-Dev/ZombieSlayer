86ec1d20a5c4cd8d5242080eca66873ba4b7229b
JerryBohenek <jerzybochenek10@gmail.com>
2020-01-05 19:29:32 +0100

    Drobne zmiany

---
+++ Assets/Scripts/DieAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieAfterTime : MonoBehaviour
+{
+    public float time;
+    private float timer;
+    private void Start()
+    {
+        timer = time;
+    }
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
