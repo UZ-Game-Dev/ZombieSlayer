9878335479c310138d85449a9ef0c0faa6bcf6e1
Bartosz Gabryjołek <b.gabryjolek@gmail.com>
2020-01-07 10:59:55 +0100

    zniszczenie obiektu po czasie - time -

---
+++ Assets/ZOS-15.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+
+    public int time;
+    
+    void DestroyObjectDelayed()
+    {
+        Destroyer(gameObject, time);
+    }
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
