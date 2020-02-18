6a5587e522f5fb76a62be5c8424251a7fca2ca60
KSSPStudio <szymon.porada@wp.pl>
2020-02-18 19:51:00 +0100

    deathscreen

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,17 +7,31 @@ using UnityEngine.UI;
 public class Main : MonoBehaviour
 {
     public Text healthText;
+    public static bool GameOver = false;
+
+    public GameObject gameoverUI;
 
 
     public void setHealth(int health)
     {
         if (health < 0) 
-        { 
+        {
+
+            if (GameOver)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
             health = 0;
-            StartCoroutine(LoadLevelAsync(SceneManager.GetActiveScene().buildIndex));
+            //StartCoroutine(LoadLevelAsync(SceneManager.GetActiveScene().buildIndex));
         }
         healthText.text = "Health: " + health;
     }
+
 
     IEnumerator LoadLevelAsync(int nr)
     {
@@ -29,4 +43,35 @@ public class Main : MonoBehaviour
             yield return null;
         }
     }
+
+
+    public void Resume()
+    {
+        gameoverUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameOver = false;
+    }
+
+
+    void Pause()
+    {
+        gameoverUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameOver = true;
+
+    }
+    public void RetryGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+
+
+
 }
